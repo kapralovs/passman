4da045ce771da3ae13870fd6223d2c2d6710cf76
kasean <ser-kapralov@yandex.ru>
2025-06-07 23:15:30 +0300

    Remove old code

--- internal/models/account/account.go
+++
@@ -1,8 +0,0 @@
-package account
-
-type AccountInfo struct {
-	ID       int64  `json:"id,omitempty"`
-	Name     string `json:"name,omitempty"`
-	Login    string `json:"login,omitempty"`
-	Password string `json:"password,omitempty"`
-}
